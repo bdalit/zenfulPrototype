134024029b87e7cb5ea489237c19f43fbf6c2da3
Kimon Monokandilos <kimonmono986@gmail.com>
2023-07-09 20:18:15 -0700

    updated

--- application/frontend/zenful/src/components/SignUpForm/SignUpForm.js
+++ application/frontend/zenful/src/components/SignUpForm/SignUpForm.js
@@ -1,33 +1,37 @@
-import { useState } from "react";
 import './SignUpForm.css'
 
-export default function Multiple() {
-  const [formData, setFormData] = useState({name: "",email: "",message: ""});
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
-    );
-};
-
+const SignUpForm = () => {
   return (
-    <form className="signupform" onSubmit={handleSubmit}>
-      <label htmlFor="name">username</label>
-      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+    <form className="signupform" id="form" method="POST" action="application/backend/routes/users/register" encType="application/x-www-form-urlencoded">
+      <label htmlFor="username">Username</label>
+      <input id="username" required="" type="text" name="username" />
         
-      <label htmlFor="email">email</label>
-      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+      <label htmlFor="email">Email</label>
+      <input required="" type="email" name="email" />
 
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" value={formData.message} onChange={handleChange}/>
+      <label htmlFor="password">Password</label>
+        <input 
+          id="password"
+          required=""
+          type="password"
+          name="password"
+        />
 
-      <button type="submit">Submit</button>
+      <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          id="password1"
+          required=""
+          type="password"
+          name="confirmPassword"
+        />
+        
+      <button id="sub" type="submit" onClick='formValidation()'>
+        Submit
+      </button>
+     
     </form>
   );
 }
+
+export default SignUpForm;
 
